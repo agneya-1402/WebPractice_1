0a9ccc7be5268ae5c6991d5dd72573993eee719b
Agneya Pathare <agneyapathare621@gmail.com>
2022-07-24 12:29:40 +0530

    added Backend and React App

---
+++ react_1/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 style={{}}>
+          Hello World
+        </h1>
+        <p>
+          React.js 
+        </p>
+        <p>
+          My first React Webapp
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
